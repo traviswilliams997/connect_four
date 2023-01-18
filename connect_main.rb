91a170cea21f930780c5f33a261ab1c8566048f6
Travis Williams <traviswilliams997@gmail.com>
2023-01-18 13:24:30 -0500

    Add tests and methods for Game and Player

--- connect_main.rb
+++ connect_main.rb
@@ -1,9 +1,37 @@
 class Game 
 
-    def initialize
-        @board = Array.new(6)
-    
-    end
+  def initialize
+    @board = create_board
+    @player_one = Player.new
+    @player_two = Player.new
+  end
 
+  def create_board 
+    [
+        {col1: 'O', col2: 'O', col3: 'O', col4: 'O', col5: 'O', col6: 'O', col7: 'O'},
+        {col1: 'O', col2: 'O', col3: 'O', col4: 'O', col5: 'O', col6: 'O', col7: 'O'},
+        {col1: 'O', col2: 'O', col3: 'O', col4: 'O', col5: 'O', col6: 'O', col7: 'O'},
+        {col1: 'O', col2: 'O', col3: 'O', col4: 'O', col5: 'O', col6: 'O', col7: 'O'},
+        {col1: 'O', col2: 'O', col3: 'O', col4: 'O', col5: 'O', col6: 'O', col7: 'O'},
+        {col1: 'O', col2: 'O', col3: 'O', col4: 'O', col5: 'O', col6: 'O', col7: 'O'}
+        ]
+  end
 
+  def print_board
+      @board.each do |row|
+        row.each {|key, value| print " #{value} " } 
+        puts ""
+      end
+  end
 end
+
+class Player
+
+    def initialize
+        @score = 0
+    end
+end
+
+new_game = Game.new
+
+new_game.print_board
--- connect_main_spec.rb
+++ connect_main_spec.rb
@@ -5,11 +5,48 @@ RSpec.describe Game do
 
     describe '#intitialize' do
         subject(:game_initialize) { described_class.new }
+
         it 'Creates a new board' do 
             board = game_initialize.instance_variable_get(:@board)
             expect(board).not_to be_empty
         end
-    
+        it 'Create a player 1' do 
+            player_one = game_initialize.instance_variable_get(:@player_one)
+            expect(player_one).not_to be_nil
+        end
+        
+        it 'Create a player 2' do 
+            player_two = game_initialize.instance_variable_get(:@player_two)
+            expect(player_two).not_to be_nil
+        end
     end
 
+    describe '#print_board' do
+        subject(:game_print) { described_class.new }
+
+        it 'Displays board to screen' do
+            expect(game_print).to receive(:print).with(" O ").exactly(42).times
+            expect(game_print).to receive(:puts).with("").exactly(6).times
+            game_print.print_board
+        end
+    end
+
+
+end
+
+RSpec.describe Player do 
+
+  describe '#intitialize' do
+    subject(:player_initialize) { described_class.new }
+
+    it 'Create a player score' do 
+      score = player_initialize.instance_variable_get(:@score)
+      expect(score).to eq(0) 
+     end
+   
+  end
+  describe 'select position' do 
+     
+  end
+  
 end
